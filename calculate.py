f8617d9eb42759c76439d165cb590aaa2459ff93
Isha Fatimah <eshafatimah554@gmail.com>
2024-08-10 18:01:52 +0500

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,22 @@
+# Define two numbers
+a = 10
+b = 5
+
+# Perform addition
+addition = a + b
+print("Addition:", addition)
+
+# Perform subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Perform multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Perform division
+if b != 0:
+    division = a / b
+    print("Division:", division)
+else:
+    print("Error: Division by zero is not allowed")
